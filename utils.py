5a269ccc568e1dda169d05c0d56ff159307807f0
Temesgen Ataro <temesgen.ataro@tii.ae>
2025-07-07 11:20:26 +0400

    Add basic FM demodulation

--- utils.py
+++ utils.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import numpy as np
+import scipy as sp
 import scipy.integrate
 import numpy.typing as npt
 import matplotlib.pyplot as plt
@@ -8,6 +9,7 @@ import matplotlib.pyplot as plt
 
 plt.style.use("./computermodern.mplstyle")
 
+DBG_PLT: bool = False
 # constants
 fs: float = 2e6  # samplerate, Hz
 dt: float = 1 / fs
@@ -48,29 +50,53 @@ def gen_fm(
     else:
         phase = np.cumsum(freq)
 
-    wrapped_phase = phase % TAU
-    plt.plot(wrapped_phase)
-    plt.show()
-    plt.plot(np.cumsum(wrapped_phase))
-    plt.show()
+    if DBG_PLT:
+        plt.plot(phase)
+        plt.show()
+        plt.plot(np.cumsum(phase))
+        plt.show()
 
     sig: npt.NDArray = np.exp(-1j * phase)
 
     return sig
 
 
+def demod_fm(sig: npt.NDArray) -> npt.NDArray:
+    angle_diff: npt.NDArray = np.diff(np.angle(sig))
+    # wrap angle_diff
+    angle_diff = np.where(angle_diff > TAU / 2, 0, angle_diff)
+    angle_diff = np.where(angle_diff < -TAU / 2, 0, angle_diff)
+
+    # LPF angle_diff
+    filt_ord: int = 11
+    b, a = sp.signal.butter(
+        N=filt_ord, Wn=200e3, btype="low", analog=False, output="ba", fs=fs
+    )
+    angle_diff_lpf = sp.signal.filtfilt(b, a, angle_diff)
+
+    if DBG_PLT:
+        fig, axs = plt.subplots(nrows=4)
+        axs[0].plot(sig.real)
+        axs[1].plot(np.angle(sig))
+        axs[2].plot(angle_diff)
+        axs[3].plot(angle_diff_lpf)
+        plt.show()
+
+    return angle_diff_lpf
+
+
 def main() -> None:
     t: npt.NDArray = np.arange(0, 100_000, dtype=np.float32) * dt
+    freq = 1 * np.sin(500 * t)
 
     def test_gen_fm():
-        freq = 1 * np.sin(500 * t)
         sig = gen_fm(freq=freq)
         plt.specgram(sig, NFFT=512)
         plt.show()
 
     def test_integration():
         # just visual gutcheck that integration works
-        sig = np.sin(100_000 *t)
+        sig = np.sin(100_000 * t)
 
         fig, axs = plt.subplots(nrows=2)
 
@@ -78,8 +104,15 @@ def main() -> None:
         axs[1].plot(np.cumsum(sig))
         plt.show()
 
-    # test_integration()
-    test_gen_fm()
+    def test_demod(sig):
+        demod_fm(sig)
+        # SIG = np.fft.fftshift(np.fft.fft(sig))  # 20 * np.log10(np.abs(np.fft.fftshift(np.fft.fft(sig))))
+        # freq = np.fft.fftfreq(SIG.size, dt)
+        # plt.plot(freq, np.abs(SIG))
+        # plt.show()
+
+    sig = gen_fm(freq=freq)
+    test_demod(sig)
 
 
 if __name__ == "__main__":
