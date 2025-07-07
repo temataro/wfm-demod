29f266f84d4928a5b95ef65c68bdccc76cdd76e4
Temesgen Ataro <temesgen.ataro@tii.ae>
2025-07-07 11:29:11 +0400

    Get IQ data from iqengine.org

---
+++ get_assets.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+echo "[STATUS] Getting analog_FM_France from IQEngine.org..."
+wget https://www.iqengine.org/api/datasources/gnuradio/iqengine/analog_FM_France.sigmf-data
+wget https://www.iqengine.org/api/datasources/gnuradio/iqengine/analog_FM_France.sigmf-meta
