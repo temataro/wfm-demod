f2886c66ceca0e7f02a99be4ee3ee297c0a33463
Temesgen Ataro <atarotemesgen@gmail.com>
2025-08-17 00:25:17 +0400

    assets: Add NOAA-APT signal from website

--- get_assets.sh
+++ get_assets.sh
@@ -3,3 +3,5 @@
 echo "[STATUS] Getting analog_FM_France from IQEngine.org..."
 wget https://www.iqengine.org/api/datasources/gnuradio/iqengine/analog_FM_France.sigmf-data
 wget https://www.iqengine.org/api/datasources/gnuradio/iqengine/analog_FM_France.sigmf-meta
+echo "[STATUS] Getting NOAA-APT wav file (post WFM demod)..."
+wget https://project.markroland.com/weather-satellite-imaging/N18_4827.zip
